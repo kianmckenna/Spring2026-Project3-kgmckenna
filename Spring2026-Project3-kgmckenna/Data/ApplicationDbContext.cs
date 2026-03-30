4dcdc25912374a0c06a69e496f992ba3c9ffd8fd
Kian McKenna <kgmckenna04@gmail.com>
2026-03-30 16:04:46 -0500

    applicationdbcontext updated

--- Spring2026-Project3-kgmckenna/Data/ApplicationDbContext.cs
+++ Spring2026-Project3-kgmckenna/Data/ApplicationDbContext.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Spring2026_Project3_kgmckenna.Models;
 
 namespace Spring2026_Project3_kgmckenna.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            modelbuilder.Entity<ActorMovie>()
+                .HasIndex(am => new { am.ActorId, am.MovieId })
+                .IsUnique();
+        }
     }
 }
