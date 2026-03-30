a63eaa5934f621e55422e8826d1486985faa3aea
Kian McKenna <kgmckenna04@gmail.com>
2026-03-30 14:48:30 -0500

    Add project files.

---
+++ Spring2026-Project3-kgmckenna/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spring2026_Project3_kgmckenna.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ Spring2026-Project3-kgmckenna/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project3_kgmckenna.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
