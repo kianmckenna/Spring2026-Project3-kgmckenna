45aee62ef74bdb900c22c6b7ca08ae8cbc92215b
Kian McKenna <kgmckenna04@gmail.com>
2026-03-30 16:36:04 -0500

    scaffolding

--- Spring2026-Project3-kgmckenna/Data/ApplicationDbContext.cs
+++ Spring2026-Project3-kgmckenna/Data/ApplicationDbContext.cs
@@ -4,17 +4,22 @@ using Spring2026_Project3_kgmckenna.Models;
 
 namespace Spring2026_Project3_kgmckenna.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor> Actors { get; set; }
         public DbSet<ActorMovie> ActorMovies { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelbuilder);
+            base.OnModelCreating(modelBuilder);
 
-            modelbuilder.Entity<ActorMovie>()
+            modelBuilder.Entity<ActorMovie>()
                 .HasIndex(am => new { am.ActorId, am.MovieId })
                 .IsUnique();
         }
