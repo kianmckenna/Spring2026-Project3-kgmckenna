f22921fe19445b2c5f1fd75f975d749639214c4a
Kian McKenna <kgmckenna04@gmail.com>
2026-03-30 15:59:32 -0500

    added actor class and actor movie class

--- Spring2026-Project3-kgmckenna/Models/Actor.cs
+++ Spring2026-Project3-kgmckenna/Models/Actor.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spring2026_Project3_kgmckenna.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [StringLength(30)]
+        public string Gender { get; set; } = "";
+
+        [Range(0, 120)]
+        public int Age { get; set; }
+
+        [Display(Name = "IMDb Link")]
+        [Required]
+        [Url]
+        public string ImdbLink { get; set; } = "";
+
+        [Display(Name = "Photo")]
+        public byte[]? Photo { get; set; }
+
+        // Relationship table navigation
+        public ICollection<ActorMovie> ActorMovies { get; set; } = new List<ActorMovie>();
+    }
+}
--- Spring2026-Project3-kgmckenna/Models/ActorMovie.cs
+++ Spring2026-Project3-kgmckenna/Models/ActorMovie.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spring2026_Project3_kgmckenna.Models
+{
+    public class ActorMovie
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Actor")]
+        [Required]
+        public int ActorId { get; set; }
+
+        [Display(Name = "Movie")]
+        [Required]
+        public int MovieId { get; set; }
+
+        // Navigation properties
+        public Actor? Actor { get; set; }
+        public Movie? Movie { get; set; }
+    }
+}
